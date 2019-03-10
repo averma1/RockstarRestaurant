c6767c7dd74f53f9d41dbf1cda847c03e42c40f8
John Hunter <jhunter1@ithaca.edu>
2019-03-10 19:50:52 -0400

    added get and set name test for Restaurant

--- src/test/java/edu/ithaca/comp345/Rockstar/RestaurantTest.java
+++ src/test/java/edu/ithaca/comp345/Rockstar/RestaurantTest.java
@@ -1,4 +1,20 @@
 package edu.ithaca.comp345.Rockstar;
 
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertFalse;
+
 public class RestaurantTest {
+
+    @Test
+    public void getAndSetNameTest(){
+        Restaurant testRest = new Restaurant("Saigon Kitchen");
+        assertTrue(testRest.getName()=="Saigon Kitchen");
+        assertFalse(testRest.getName()!="Saigon Kitchen");
+
+        testRest.setName("Luna");
+        assertTrue(testRest.getName()=="Luna");
+        assertFalse(testRest.getName()!="Luna");
+    }
 }
