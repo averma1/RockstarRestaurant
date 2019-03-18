d7e6dd113a93c9205d9ba87356dafafbd21641fc
John Hunter <jhunter1@ithaca.edu>
2019-03-18 09:37:38 -0400

    Started the item class Changed restaurant class to support the stock class

--- src/main/java/edu/ithaca/comp345/Rockstar/Item.java
+++ src/main/java/edu/ithaca/comp345/Rockstar/Item.java
@@ -1,4 +1,15 @@
 package edu.ithaca.comp345.Rockstar;
 
 public class Item {
+    public String name;
+    public double cost;
+    public int quanity;
+
+    public Item(String nameIn, double costIn, int quanityIn){
+        this.name = nameIn;
+        this.cost = costIn;
+        this.quanity = quanityIn;
+    }
+
+
 }
