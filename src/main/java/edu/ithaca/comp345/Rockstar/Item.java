f0c9588f42a0a45fb75f3cf4cd91a4106db1c2ae
John Hunter <jhunter1@ithaca.edu>
2019-03-18 09:49:28 -0400

    Flushed out the Item Class, need to finish functions and tests

--- src/main/java/edu/ithaca/comp345/Rockstar/Item.java
+++ src/main/java/edu/ithaca/comp345/Rockstar/Item.java
@@ -3,13 +3,42 @@ package edu.ithaca.comp345.Rockstar;
 public class Item {
     public String name;
     public double cost;
-    public int quanity;
+    public int quantity;
 
-    public Item(String nameIn, double costIn, int quanityIn){
+    public Item(String nameIn, double costIn, int quantityIn){
         this.name = nameIn;
         this.cost = costIn;
-        this.quanity = quanityIn;
+        this.quantity = quantityIn;
     }
 
+    public String getName(){
+        return name;
+    }
 
+    public double getCost(){
+        return cost;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public boolean checkIfAvailable(int amountNeeded) {
+        //TODO
+        return false;
+    }
+
+    public void changeQuantity(int amount) { //amount can be + or -
+        //TODO check if amount is available?
+        quantity = quantity + amount;
+    }
+
+    public void changeCost(double costIn){
+        cost = costIn;
+    }
+
+    public boolean isCostValid(double costIn){
+        //TODO
+        return false;
+    }
 }
