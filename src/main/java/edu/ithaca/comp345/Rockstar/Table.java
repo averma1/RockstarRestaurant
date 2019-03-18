df77d9e0fcb66f90874c551cf3d467e9771cdf82
Emma <emurtagh03@gmail.com>
2019-03-18 09:23:16 -0400

    added Table basic functionality

--- src/main/java/edu/ithaca/comp345/Rockstar/Table.java
+++ src/main/java/edu/ithaca/comp345/Rockstar/Table.java
@@ -1,4 +1,27 @@
 package edu.ithaca.comp345.Rockstar;
 
 public class Table {
+    private int numOfSeats;
+    private int tableNumber;
+    public Table(int tableNumber, int numOfSeats){
+        this.numOfSeats= numOfSeats;
+        this.tableNumber= tableNumber;
+    }
+    public int getTableNumber(){
+        return this.tableNumber;
+    }
+    public int setTableNumber(int tableNumber){
+        this.tableNumber=tableNumber;
+        return this.tableNumber;
+    }
+
+    public int getNumOfSeats(){
+        return this.numOfSeats;
+    }
+    public int setNumOfSeats(int numOfSeats){
+        this.numOfSeats=numOfSeats;
+        return this.numOfSeats;
+    }
+
+
 }
