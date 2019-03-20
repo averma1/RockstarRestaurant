6f5f8e3cba1752660b12b80627925ccc912c81f3
John Hunter <jhunter1@ithaca.edu>
2019-03-19 20:49:30 -0400

    Added Tests for Ingredient. checkIfAvailable doesn't pass

--- src/test/java/edu/ithaca/comp345/Rockstar/IngredientTest.java
+++ src/test/java/edu/ithaca/comp345/Rockstar/IngredientTest.java
@@ -2,6 +2,10 @@ package edu.ithaca.comp345.Rockstar;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IngredientTest {
@@ -11,5 +15,13 @@ public class IngredientTest {
         assertTrue(ingredient.getName().equals("chicken"));
         assertTrue(ingredient.getCost() == 2.0);
         assertTrue(ingredient.getQuantity() == 300);
+    }
+
+    @Test
+    public void checkIfAvailableTest(){
+        Ingredient ingredient = new Ingredient("onion", 1, 10);
+        assertTrue(ingredient.checkIfAvailable(5));
+        assertTrue(ingredient.checkIfAvailable(10));
+        assertFalse(ingredient.checkIfAvailable(11));
     }
 }
