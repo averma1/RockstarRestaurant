a18b568ae1001305603ae3beea0bbc46c63b01c3
Emma <emurtagh03@gmail.com>
2019-03-20 11:13:38 -0400

    Initial commit to hostApiBranch, added needed files

---
+++ src/main/java/edu/ithaca/comp345/Rockstar/hostApi.java
@@ -0,0 +1,5 @@
+package edu.ithaca.comp345.Rockstar;
+
+public class hostApi {
+    //This is where code goes ofr hostApi
+}
---
+++ src/test/java/edu/ithaca/comp345/Rockstar/hostApiTest.java
@@ -0,0 +1,5 @@
+package edu.ithaca.comp345.Rockstar;
+
+public class hostApiTest {
+    //This is where hostApi test code goes
+}
