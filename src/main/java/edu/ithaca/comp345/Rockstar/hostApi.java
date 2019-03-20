3ffb1781fc2fea272bff4c1fb4fcda699328bec7
Aine <m.g.firefox@gmail.com>
2019-03-20 19:14:24 -0400

    push tables together

--- src/main/java/edu/ithaca/comp345/Rockstar/hostApi.java
+++ src/main/java/edu/ithaca/comp345/Rockstar/hostApi.java
@@ -4,11 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class hostApi {
-    List<Table> allTables= new ArrayList<>();
+    public static  List<Table> allTables= new ArrayList<>();
 
     public static Table pushTables(Table table1, Table table2){
+        int index1= findTable(table1);
+        int index2= findTable(table2);
 
-        return
+        if(index1!=-1 && index2!=-1){
+            int num= table1.getNumOfSeats()+table2.getNumOfSeats();
+            Table newTable= new Table(table1.getTableNumber(), num);
+            return newTable;
+        }
+        else{
+            throw new IndexOutOfBoundsException();
+        }
     }
 
     public static Table splitTable(Table table){
@@ -38,6 +47,16 @@ public class hostApi {
 
     public static void searchTableBySize(int size){
 
+    }
+
+    private static int findTable(Table table){
+        int index=-1;
+        for(int i=0; i<allTables.size(); i++){
+            if(table.getTableNumber()==allTables.get(i).getTableNumber()){
+                index=i;
+            }
+        }
+        return index;
     }
 
 }
