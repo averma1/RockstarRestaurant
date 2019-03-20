269e4960a5a9653120b88bd22fc227efce59281f
Aine <m.g.firefox@gmail.com>
2019-03-20 17:34:37 -0400

    hostAPI base classes

--- src/main/java/edu/ithaca/comp345/Rockstar/hostApi.java
+++ src/main/java/edu/ithaca/comp345/Rockstar/hostApi.java
@@ -1,5 +1,43 @@
 package edu.ithaca.comp345.Rockstar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class hostApi {
-    //This is where code goes ofr hostApi
+    List<Table> allTables= new ArrayList<>();
+
+    public static Table pushTables(Table table1, Table table2){
+
+        return
+    }
+
+    public static Table splitTable(Table table){
+
+        return
+    }
+
+    public static void removeTable(Table table){
+
+    }
+
+    public static void clearTable(Table table){
+
+    }
+
+    public static void seatCustomers(Table table, int numOfPeople){
+
+    }
+
+    public static void viewAllTables(){
+
+    }
+
+    public static void printTabledata(Table table){
+
+    }
+
+    public static void searchTableBySize(int size){
+
+    }
+
 }
