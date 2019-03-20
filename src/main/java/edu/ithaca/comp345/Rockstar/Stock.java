288160ece4a7b068412c96ed20f6e25a4d707c78
Peter Mankiewich <pmank64@gmail.com>
2019-03-19 20:41:21 -0400

    stock class created and tests written

--- src/main/java/edu/ithaca/comp345/Rockstar/Stock.java
+++ src/main/java/edu/ithaca/comp345/Rockstar/Stock.java
@@ -1,5 +1,49 @@
 package edu.ithaca.comp345.Rockstar;
 
+
+import java.util.HashMap;
+
 public class Stock {
-    //stock
+    private HashMap<String, Ingredient> ingredientMap;
+
+    public Stock(){
+        ingredientMap = new HashMap<>();
+    }
+
+    public void addIngredient(String name, double cost, int quantity){
+        ingredientMap.put(name, new Ingredient(name, cost, quantity));
+    }
+
+    public Ingredient getIngredient(String nameToFind){
+        return ingredientMap.get(nameToFind);
+    }
+
+    public double getCost(String nameToFind){
+        return ingredientMap.get(nameToFind).getCost();
+    }
+
+    public int getQuantity(String nameToFind){
+        return ingredientMap.get(nameToFind).getQuantity();
+    }
+
+    public void removeIngredient(String nameToFind){
+        //TODO
+    }
+
+    public void changeIngredientName(String nameToFind){
+        //TODO
+    }
+
+    public void changeIngredientCost(String nameToFind){
+        //TODO
+    }
+
+    public void changeIngredientQuantity(String nameToFind){
+        //TODO
+    }
+
+    public HashMap getIngredientsList(){
+        return ingredientMap;
+    }
+
 }
--- src/test/java/edu/ithaca/comp345/Rockstar/IngredientTest.java
+++ src/test/java/edu/ithaca/comp345/Rockstar/IngredientTest.java
@@ -1,5 +1,15 @@
 package edu.ithaca.comp345.Rockstar;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class IngredientTest {
-    //Tests
+    @Test
+    public void createIngredientTest(){
+        Ingredient ingredient = new Ingredient("chicken", 2.0, 300);
+        assertTrue(ingredient.getName().equals("chicken"));
+        assertTrue(ingredient.getCost() == 2.0);
+        assertTrue(ingredient.getQuantity() == 300);
+    }
 }
