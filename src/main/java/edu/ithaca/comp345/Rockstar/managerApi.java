9b65a81533a7c6084070bd77840e7d6e51eba10c
John Hunter <jhunter1@ithaca.edu>
2019-03-21 19:33:17 -0400

    Tests for managerApi checkIfAvailable and removeIngredient Tests currently fail

--- src/main/java/edu/ithaca/comp345/Rockstar/managerApi.java
+++ src/main/java/edu/ithaca/comp345/Rockstar/managerApi.java
@@ -4,21 +4,28 @@ public class managerApi {
     //TODO this will be changed to the stock of a real restaurant
     Stock managerStock = new Stock();
 
-    public void addIngredient(String name, double cost, int quantity){
+    public static void addIngredient(String name, double cost, int quantity){
 
     }
 
-    public Ingredient getIngredient(String nameToFind){
+    public static void removeIngredient(String name){
+
+    }
+
+    public static Ingredient getIngredient(String nameToFind){
         return new Ingredient("asd", 123,123);
     }
 
-    public double getCostOfIngredient(String nameToFind){
+    public static double getCostOfIngredient(String nameToFind){
         return -11;
     }
 
-    public int getQuantityOfIngredient(String nameToFind){
+    public static int getQuantityOfIngredient(String nameToFind){
         return -11;
     }
 
-    
+    public static boolean isIngredientAvailable(String ingredientName){
+        return false;
+    }
+
 }
---
+++ src/test/java/edu/ithaca/comp345/Rockstar/managerApiTest.java
@@ -0,0 +1,49 @@
+package edu.ithaca.comp345.Rockstar;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class managerApiTest {
+
+    @Test
+    public void addAndGetIngredientTest(){
+        //add Ingredients
+        managerApi.addIngredient("chicken", 9.50, 30);
+        managerApi.addIngredient("potatoes", 1.30, 50);
+        managerApi.addIngredient("onion", 2.00, 20);
+
+        //test getIngredient
+        Ingredient testIngredient1 = managerApi.getIngredient("chicken");
+        Ingredient testIngredient2 = managerApi.getIngredient("potatoes");
+        Ingredient testIngredient3 = managerApi.getIngredient("pickels");
+
+        assertTrue(testIngredient1.getName() == "chicken");
+        assertTrue(testIngredient1.getQuantity() == 30);
+        assertTrue(testIngredient1.getCost() == 9.5);
+    }
+
+    @Test
+    public void checkIfAvailableAndRemoveIngredientTest(){
+        //add Ingredients
+        managerApi.addIngredient("chicken", 9.50, 30);
+        managerApi.addIngredient("potatoes", 1.30, 50);
+        managerApi.addIngredient("onion", 2.00, 20);
+
+        //check if Available
+        assertTrue(managerApi.isIngredientAvailable("chicken"));
+        assertFalse(managerApi.isIngredientAvailable("pickels"));
+
+        //remove Ingredients
+        managerApi.removeIngredient("chicken");
+
+        //check if Available
+        assertFalse(managerApi.isIngredientAvailable("chicken"));
+    }
+
+
+}
