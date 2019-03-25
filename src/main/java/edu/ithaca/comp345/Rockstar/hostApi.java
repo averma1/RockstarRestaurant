ac971a543c7728ebf9e6233cea3b5fff220dcdf2
averma1 <anika_verma@yahoo.com>
2019-03-24 21:39:02 -0400

    hostApi javadoc added

--- src/main/java/edu/ithaca/comp345/Rockstar/hostApi.java
+++ src/main/java/edu/ithaca/comp345/Rockstar/hostApi.java
@@ -4,14 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class hostApi {
+
     public static  List<Table> allTables= new ArrayList<>();
     public static  List<MultiTable> MultiTables= new ArrayList<>();
 
+    /**
+     * @param tableNumber: table number of the table to create
+     * @param numOfSeats: number of seats at the table to create
+     * creates a table
+     */
     public static void createTable(int tableNumber, int numOfSeats){
         Table table= new Table(tableNumber, numOfSeats);
         allTables.add(table);
     }
 
+    /**
+     * @param table1: first table to push
+     * @param table2: second table to push against first table
+     * @return Multitable: creates a multitable and returns this table
+     */
     public static MultiTable pushTables(int table1, int table2){
         int index1= findTable(table1);
         int index2= findTable(table2);
@@ -29,7 +40,11 @@ public class hostApi {
             throw new IndexOutOfBoundsException();
         }
     }
-
+    /**
+     * @param tableNum: table number of the table to split
+     * splits the table that was pushed together
+     * after this is run, there should be 2 tables once again
+     */
     public static void splitTable(int tableNum){
         int index= findTable(tableNum);
         if(allTables.get(index).isMultiTable){
@@ -45,17 +60,30 @@ public class hostApi {
         }
     }
 
+    /**
+     * @param tableNum: table number of the table to remove
+     * removes a table from master list of tables
+     */
     public static void removeTable(int tableNum){
         int index=findTable(tableNum);
         allTables.remove(index);
     }
 
+    /**
+     * @param tableNum: table number of the table to clear
+     * clears the table: sets the number of seats filled at that table back to 0
+     */
     public static void clearTable(int tableNum){
         int index=findTable(tableNum);
         allTables.get(index).setNumOfSeatsFilled(0);
         allTables.get(index).clearTable();
     }
 
+    /**
+     * @param tableNum: table number of the table to seat people at
+     * @param numOfPeople: number of people to seat
+     * seats customers to a certain table: sets the number of seats filled to the number of people to seat
+     */
     public static void seatCustomers(int tableNum, int numOfPeople){
         int index=findTable(tableNum);
         if(numOfPeople<allTables.get(index).getNumOfSeats() && allTables.get(index).isTableEmpty()) {
@@ -67,6 +95,11 @@ public class hostApi {
 
     }
 
+
+    /**
+     * prints out all the tables
+     * @return returns the number of tables there are in the master table list
+     */
     public static int viewAllTables(){
         int myTableNum = 0;
         int count = 1;
@@ -78,11 +111,20 @@ public class hostApi {
         return count;
     }
 
+    /**
+     * @param tableNum: table number to print data for
+     * prints all the data for the specific table entered
+     */
     public static void printTableData(int tableNum){
         int index=findTable(tableNum);
         System.out.println(("Table number: "+allTables.get(index).getTableNumber()+", Number of Seats: "+allTables.get(index).getNumOfSeats()+", Available: "+allTables.get(index).isTableEmpty()));
     }
 
+    /**
+     * @param size: size of the table
+     * searches the entire table list to find tables that are of the size specified
+     * @return returns a list of tables that are of the size specified
+     */
     public static ArrayList<Table> searchTableBySize(int size){
         ArrayList<Table> tablesOfSize = new ArrayList<>();
         for(int i = 0; i < allTables.size(); i++){
@@ -93,6 +135,11 @@ public class hostApi {
         return tablesOfSize;
     }
 
+    /**
+     * finds the position of the table in the master list of tables
+     * @param table:table number of the table
+     * @return returns the index of where the table number is at
+     */
     public static int findTable(int table){
         int index=-1;
         for(int i=0; i<allTables.size(); i++){
@@ -103,6 +150,11 @@ public class hostApi {
         return index;
     }
 
+    /**
+     * returns the position of the table in the list of multi tables
+     * @param table: table number of the multi table
+     * @return returns the index of where the table number is at
+     */
     public static int findInMulti(int table){
         int index=-1;
         for(int i=0; i<MultiTables.size(); i++){
