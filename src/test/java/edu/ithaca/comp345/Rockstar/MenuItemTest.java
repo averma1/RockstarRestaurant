eb6b1ed12a7a74b2a302d6b5e7c4c7d7c5854d72
Peter Mankiewich <pmank64@gmail.com>
2019-03-28 10:04:58 -0400

    Created MenuItem and MenuItemTest classes

---
+++ src/test/java/edu/ithaca/comp345/Rockstar/MenuItemTest.java
@@ -0,0 +1,14 @@
+package edu.ithaca.comp345.Rockstar;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class MenuItemTest {
+
+}
