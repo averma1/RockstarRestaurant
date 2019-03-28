4cbb6ced5e628f96a8b2b49f4d6cc698cced984f
Peter Mankiewich <pmank64@gmail.com>
2019-03-28 10:20:02 -0400

    Adds JavaDocs for MenuItem

--- src/main/java/edu/ithaca/comp345/Rockstar/MenuItem.java
+++ src/main/java/edu/ithaca/comp345/Rockstar/MenuItem.java
@@ -12,26 +12,50 @@ public class MenuItem {
 
     }
 
+    /**
+     * adds an ingredient object to the list of ingredients for this menu item
+     * @param ingredientToAdd the Ingredient object to add
+     */
     public void addIngredient(Ingredient ingredientToAdd){
 
     }
 
+    /**
+     * removes an ingredient from the list of ingredients for this menu item
+     * @param ingredientToRemove the ingredient object to remove
+     */
     public void removeIngredient(Ingredient ingredientToRemove){
 
     }
 
-    public void changePrice(String itemName){
+    /**
+     * changes the price of the menu item
+     * @param newPrice the new price to set for the menuItem
+     */
+    public void changePrice(double newPrice){
 
     }
 
+    /**
+     * gets the list of ingredients of the menu item
+     * @return the list of ingredients
+     */
     public ArrayList<Ingredient> getIngredients(){
         return ingredients;
     }
 
+    /**
+     * gets the price of the menu item
+     * @return the price of the menu item
+     */
     public double getPrice(){
         return 0.0;
     }
 
+    /**
+     * gets the name of the menu item
+     * @return the name of the menu item
+     */
     public String getItemName(){
         return "";
     }
