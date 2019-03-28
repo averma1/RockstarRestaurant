673ff7feb3907481868b02bb92f44d718c6da436
Peter Mankiewich <pmank64@gmail.com>
2019-03-28 10:38:02 -0400

    Add Ingredient Test

--- src/test/java/edu/ithaca/comp345/Rockstar/MenuItemTest.java
+++ src/test/java/edu/ithaca/comp345/Rockstar/MenuItemTest.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MenuItemTest {
 
@@ -25,7 +24,37 @@ public class MenuItemTest {
         assertEquals(25.00, item1.getPrice());
         assertEquals("chicken dish", item1.getItemName());
         assertEquals(ingredients, item1.getIngredients());
+    }
 
+    @Test
+    public void addIngredientTest(){
+
+        Ingredient i1 = new Ingredient("chicken", 1.00, 1);
+        Ingredient i2 = new Ingredient("mushrooms", 2.20, 5);
+        Ingredient i3 = new Ingredient("oregano", 0.50, 8);
+
+        ArrayList<Ingredient> ingredients = new ArrayList<>();
+
+        MenuItem item1 = new MenuItem(ingredients, 25.00, "chicken dish");
+
+        //check that the current items are there
+        assertEquals(3, item1.getIngredients().size());
+        assertEquals("chicken", item1.getIngredients().get(0).getName());
+        assertEquals("mushrooms", item1.getIngredients().get(1).getName());
+        assertEquals("oregano", item1.getIngredients().get(2).getName());
+
+        //ingredient to add
+        Ingredient i4 = new Ingredient("pepper", 0.2, 3);
+
+        //add the ingredient
+        item1.addIngredient(i4);
+
+        //check that all the items are there
+        assertEquals(4, item1.getIngredients().size());
+        assertEquals("chicken", item1.getIngredients().get(0).getName());
+        assertEquals("mushrooms", item1.getIngredients().get(1).getName());
+        assertEquals("oregano", item1.getIngredients().get(2).getName());
+        assertEquals("pepper", item1.getIngredients().get(3).getName());
     }
 
 }
