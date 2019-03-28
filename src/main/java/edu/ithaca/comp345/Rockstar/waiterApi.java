53a9e5526d0c9a80f4acf0ab25df6057f3c96a5c
Emma <emurtagh03@gmail.com>
2019-03-28 10:43:03 -0400

    Finished first tests of takeOrderTest and payTotalBillTest, and added function skeletons in waiterApi.

---
+++ src/main/java/edu/ithaca/comp345/Rockstar/waiterApi.java
@@ -0,0 +1,10 @@
+package edu.ithaca.comp345.Rockstar;
+
+public class waiterApi {
+    public void takeOrder(){}
+    public void payTotalBill(){}
+    public void splitBillByTotal(){}
+    public void splitTotalByItem(){}
+    public void viewOrder(){}
+}
+
