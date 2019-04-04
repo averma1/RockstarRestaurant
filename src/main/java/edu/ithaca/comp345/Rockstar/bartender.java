4dc1bfb96784ed30a5c870fa6b356cf1a50efb4a
Emma <emurtagh03@gmail.com>
2019-04-04 11:28:07 -0400

    Setup bartenderBranch, added skeletons for bartender and bartenderTest classes

---
+++ src/main/java/edu/ithaca/comp345/Rockstar/bartender.java
@@ -0,0 +1,13 @@
+package edu.ithaca.comp345.Rockstar;
+
+public class bartender {
+    public void placeOrder(){}
+
+    public void menu(){}
+
+    public void pay(){}
+
+    public void tab(){}
+
+    public void stock(){}
+}
---
+++ src/test/java/edu/ithaca/comp345/Rockstar/bartenderTest.java
@@ -0,0 +1,22 @@
+package edu.ithaca.comp345.Rockstar;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class bartenderTest {
+
+    @Test
+    public void placeOrderTest(){}
+
+    @Test
+    public void menuTest(){}
+
+    @Test
+    public void payTest(){}
+
+    @Test
+    public void tabTest(){}
+
+    @Test
+    public void stockTest(){}
+}
