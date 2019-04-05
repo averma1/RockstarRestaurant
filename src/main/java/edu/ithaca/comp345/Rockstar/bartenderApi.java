8d0692fb1efde9094fbe746b2deb999ecc11ec01
Aine <m.g.firefox@gmail.com>
2019-04-05 16:58:52 -0400

    bartender skellington updated function parameters

--- src/main/java/edu/ithaca/comp345/Rockstar/bartenderApi.java
+++ src/main/java/edu/ithaca/comp345/Rockstar/bartenderApi.java
@@ -1,18 +1,19 @@
 package edu.ithaca.comp345.Rockstar;
 
+
 public class bartenderApi extends waiterApi{
     public int seats;
     public Table bar;
     public Stock stock;
     //public Menu menu;
 
-    public bartenderApi(){};
+    public bartenderApi(){}
 
-    public void createOrder(){}
+    public void createOrder(int orderNum, MenuItem item){}
 
-    public void addToOrder(){}
+    public void addToOrder(int orderNum, MenuItem item){}
 
-    public void pay(){}
+    public void pay(int orderNum){}
 
     public static void setSeats(int seats){}
 
