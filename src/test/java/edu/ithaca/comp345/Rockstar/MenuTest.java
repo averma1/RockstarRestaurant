6685de0e6dd3ad17be6566084879b089f30c9014
Peter Mankiewich <pmank64@gmail.com>
2019-04-08 12:41:02 -0400

    remove menu item test

--- src/test/java/edu/ithaca/comp345/Rockstar/MenuTest.java
+++ src/test/java/edu/ithaca/comp345/Rockstar/MenuTest.java
@@ -1,6 +1,8 @@
 package edu.ithaca.comp345.Rockstar;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 
@@ -52,5 +54,49 @@ public class MenuTest {
         assertEquals("duck", menu.getMenuItemMap().get("New Dish").getIngredients().get(2).getName());
 
     }
+
+    @Test
+    public void removeMenuItemTest(){
+
+        //create the Ingredient objects
+        Ingredient i1 = new Ingredient("chicken", 2.0, 301);
+        Ingredient i2 = new Ingredient("fish", 3.40, 400);
+        Ingredient i3 = new Ingredient("duck", 10.0, 30);
+
+        //create the ArrayList of Ingredient objects and add the objects to the list
+        ArrayList<Ingredient> ingredients = new ArrayList<>();
+        ingredients.add(i1);
+        ingredients.add(i2);
+        ingredients.add(i3);
+
+        //create a new Stock and Menu object
+        Stock stock = new Stock();
+        Menu menu = new Menu("Spring Menu", stock);
+
+        //create a new MenuItem with the list of ingredients and add it to the menu
+        MenuItem item = new MenuItem(ingredients, 4.0, "new dish");
+        MenuItem item2 = new MenuItem(ingredients, 4.3, "the other dish");
+        MenuItem item3 = new MenuItem(ingredients, 4.3, "the best dish");
+        menu.addMenuItem(item);
+        menu.addMenuItem(item2);
+        menu.addMenuItem(item3);
+
+        //verify the MenuItem exist
+        assertTrue(menu.getMenuItemMap().containsKey("New Dish"));
+        assertTrue( menu.getMenuItemMap().containsKey("the other dish"));
+        assertTrue(menu.getMenuItemMap().containsKey("the best dish"));
+
+        //remove an item
+        menu.removeMenuItem("the other dish");
+
+        //verify the MenuItem was removed
+        assertTrue(menu.getMenuItemMap().containsKey("New Dish"));
+        assertFalse(menu.getMenuItemMap().containsKey("the other dish"));
+        assertTrue(menu.getMenuItemMap().containsKey("the best dish"));
+
+
+    }
+
+
 
 }
