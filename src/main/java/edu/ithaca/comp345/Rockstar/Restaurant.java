82f45e007333921b17d402ca4365e92e1e39a7c8
John Hunter <jhunter1@ithaca.edu>
2019-04-17 10:43:32 -0400

    Added more tests to loadTablesFromFile, currently fail

--- src/main/java/edu/ithaca/comp345/Rockstar/Restaurant.java
+++ src/main/java/edu/ithaca/comp345/Rockstar/Restaurant.java
@@ -1,5 +1,9 @@
 package edu.ithaca.comp345.Rockstar;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -56,7 +60,27 @@ public class Restaurant {
         return index;
     }
 
-    public void loadTablesFromFile(String fileName){
+    public void loadTablesFromFile(String fileName) throws Exception{
+        BufferedReader br = null;
 
+        try
+        {
+            File file = new File(fileName);
+            br = new BufferedReader(new FileReader(file));
+        }
+        catch (FileNotFoundException e) {
+            throw new FileNotFoundException("The file " + fileName + " doesn't exist!");
+        }
+
+        String st;
+        while ((st = br.readLine()) != null){
+            try {
+                Table newTable = new Table(Integer.parseInt(st.substring(0,st.indexOf(","))), Integer.parseInt(st.substring(st.indexOf(",")+1)));
+                allTables.add(newTable);
+            }
+            catch(StringIndexOutOfBoundsException | NumberFormatException e){
+                System.out.println("Invalid Input: " + st);
+            }
+        }
     }
 }
--- src/test/java/edu/ithaca/comp345/Rockstar/RestaurantTest.java
+++ src/test/java/edu/ithaca/comp345/Rockstar/RestaurantTest.java
@@ -22,7 +22,7 @@ class RestaurantTest {
         assertFalse(testRest.getName()!="Luna");
     }
 
-    @Test void loadTablesFromFileTests(){
+    @Test void loadTablesFromFileTests() throws Exception{
         Restaurant testRest = new Restaurant("My Restaurant");
 
         //the restaurant currently has no tables
@@ -34,11 +34,17 @@ class RestaurantTest {
         //confirm that the tables are created
         assertTrue(testRest.allTables.get(0).getTableNumber() == 1);
         assertTrue(testRest.allTables.get(0).getNumOfSeats() == 4);
-        assertTrue(testRest.allTables.get(0).getTableNumber() == 2);
-        assertTrue(testRest.allTables.get(0).getNumOfSeats() == 8);
-        assertTrue(testRest.allTables.get(0).getTableNumber() == 3);
-        assertTrue(testRest.allTables.get(0).getNumOfSeats() == 6);
-        assertTrue(testRest.allTables.get(0).getTableNumber() == 4);
-        assertTrue(testRest.allTables.get(0).getNumOfSeats() == 4);
+        assertTrue(testRest.allTables.get(1).getTableNumber() == 2);
+        assertTrue(testRest.allTables.get(1).getNumOfSeats() == 8);
+        assertTrue(testRest.allTables.get(2).getTableNumber() == 3);
+        assertTrue(testRest.allTables.get(2).getNumOfSeats() == 6);
+        assertTrue(testRest.allTables.get(3).getTableNumber() == 4);
+        assertTrue(testRest.allTables.get(3).getNumOfSeats() == 4);
+
+        //try to load the same file again (shouldn't create any tables due to duplicate table numbers)
+        testRest.loadTablesFromFile("tableTestFile1.txt");
+        System.out.println(testRest.allTables.size());
+        //assertTrue();
+
     }
 }
