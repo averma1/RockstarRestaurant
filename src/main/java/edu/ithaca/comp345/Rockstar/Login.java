afa6833b98edc96004db62e44b6a34b79b07d943
Aine <m.g.firefox@gmail.com>
2019-04-18 10:04:51 -0400

    test if pin is valid

--- src/main/java/edu/ithaca/comp345/Rockstar/Login.java
+++ src/main/java/edu/ithaca/comp345/Rockstar/Login.java
@@ -3,20 +3,34 @@ package edu.ithaca.comp345.Rockstar;
 public class Login extends Restaurant {
 
     public Login(int pin){
-        String mode1= pins.get(pin);
-        UIMode mode = UIMode.valueOf(mode1);
+        if(isValidPin(pin)) {
+            String mode1 = pins.get(pin);
+            UIMode mode = UIMode.valueOf(mode1);
 
-        if (mode == UIMode.manager) {
-            //call manager UI
-        } else if (mode == UIMode.host){
-            //call host UI
-        }else if (mode == UIMode.waiter){
-            //call waiter UI
-        }else if (mode == UIMode.bartender){
-            //call bartender UI
+            if (mode == UIMode.manager) {
+                //call manager UI
+            } else if (mode == UIMode.host) {
+                //call host UI
+            } else if (mode == UIMode.waiter) {
+                //call waiter UI
+            } else if (mode == UIMode.bartender) {
+                //call bartender UI
+            } else {
+                throw new RuntimeException("UI entered invalid mode.");
+            }
         } else {
-            throw new RuntimeException("UI entered invalid mode.");
+            throw new RuntimeException("Invalid pin");
         }
     }
-    
+
+    public boolean isValidPin(int pin){
+        int length = (int) (Math.log10(pin) + 1);
+        if(length==4) {
+            if (pins.containsKey(pin)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
