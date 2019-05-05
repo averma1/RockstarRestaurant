7bb9e1b4b2ba067e200194d466a473603ad16c66
Emma <emurtagh03@gmail.com>
2019-05-05 11:33:00 -0400

    Updated login GUI to look better.

--- src/test/java/edu/ithaca/comp345/Rockstar/SwingTestUtil.java
+++ src/test/java/edu/ithaca/comp345/Rockstar/SwingTestUtil.java
@@ -1,11 +1,12 @@
 package edu.ithaca.comp345.Rockstar;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SwingTestUtil {
 
     public static void showPanelInTestFrame(JPanel panelToTest){
-        JFrame testFrame = new JFrame();
+        JFrame testFrame = new JFrame("Rockstar Restaurant GUI");
         testFrame.add(panelToTest);
         testFrame.pack();
         testFrame.setExtendedState(testFrame.MAXIMIZED_BOTH);
